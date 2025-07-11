f869b53c4458206019cba8a8a2e068576f398ab2
Kenny Gabriel Perez Rosales <121875673+KennyGi0053@users.noreply.github.com>
2025-07-11 13:40:10 -0400

    Primer commit limpio sin archivos pesados

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from '../components/Navbar'
+import Calculadora from '../components/Calculadora'
+import Formulario from '../components/Formulario'
+function App() {
+
+
+  return (
+  <div>
+  <Navbar/>  
+  <Calculadora/>
+  <Formulario/>
+  </div>
+  )
+}
+
+export default App
